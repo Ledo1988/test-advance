a869ac1e415169a599c8dcb51810d7708ad04081
Elena <elena.do1988@yandex.ru>
2020-08-26 15:23:52 +0300

    3nd step - methods

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./styles/main.css";
 
 const inputTypeArray = [
-	{value: "Phone", type: "tel"},
-	{value: "Mail", type: "email"},
-	{value: "Link", type: "url"},
+	{value: "phone", type: "tel"},
+	{value: "email", type: "email"},
+	{value: "link", type: "url"},
 ]
 
 class App extends React.Component {
@@ -12,12 +12,12 @@ class App extends React.Component {
 		super(props);
 		this.state = {
 			data: [
-				{id: 0, type: "Phone", value: "", inputType: "tel"},
+				{id: 0, type: "phone", value: "", inputType: "tel", pattern: "^[0-9-+\\s()]*$"},
 			]
 		};
 
 		//this.handleChange = this.handleChange.bind(this);
-		//this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
 	handleChange(event, i) {
@@ -30,8 +30,8 @@ class App extends React.Component {
 
 		let item = data.find(item => item.id === itemId);
 		item.type = itemValue;
-		//item.value = "";
 		item.inputType = currentInputType;
+		(currentInputType === 'tel') ? item.pattern = "^[0-9-+\\s()]*$" : item.pattern = "";
 
 		this.setState({data});
 	}
@@ -50,7 +50,7 @@ class App extends React.Component {
 	handleAddInput (i) {
 		const data = [...this.state.data];
 		data.forEach(item => (item.id > i) ? item.id++ : false);
-		data.push({id: i + 1, type: "Phone", value: "", inputType: "tel"});
+		data.push({id: i + 1, type: "phone", value: "", inputType: "tel", pattern: "^[0-9-+\\s()]*$"});
 		data.sort(function (a, b) {
 			if (a.id > b.id) {
 				return 1;
@@ -61,7 +61,7 @@ class App extends React.Component {
 
 			return 0;
 		});
-		//data.push({id: data.length, type: "Phone", value: "", inputType: "tel"});
+
 		this.setState({data});
 	}
 
@@ -72,48 +72,97 @@ class App extends React.Component {
 		this.setState({data});
 	}
 
+
+
 	handleSubmit(event) {
-		alert("Your choice: " + this.state.data.type + " " + this.state.data.value);
+		//alert("Your choice: " + this.state.data.type + " " + this.state.data.value);
 		event.preventDefault();
+
+		let data = [...this.state.data];
+		data = data.filter(item => item.value !== "");
+
+		const dataType = data.map(item => item.type);
+		const dataValue = data.map(item => item.value);
+
+		const newObject = new getFormValues(dataType, dataValue);
+
+		function getFormValues(type, value) {
+			this.type = type;
+			this.value = value;
+		}
+		console.log(newObject)
+
+		const result = convertArrayToObject (newObject);
+
+		function convertArrayToObject (object) {
+			let result = Object.keys(object).map(function(e, i) {
+				let obj = {};
+				console.log(Object.keys(object));
+				Object.keys(object).forEach((a, j) => obj[a] = object[a][i]);
+				return obj;
+			})
+
+			return result;
+		}
+
+		console.log(result)
 	}
+
+
 
 	render() {
 		return (
-			<form onSubmit={this.handleSubmit} method="POST">
+			<form onSubmit={this.handleSubmit} method="POST" action="/">
 				{this.state.data.map((item, i) => {
 					return (
 					<div key={i} className="main-form__item">
-						<label>
+						<label className="main-form__label">
 							<select value={item.type}
 									onChange={event => this.handleChange(event, i)}
 									className="main-form__select">
-								<option value="Phone"
+								<option value="phone"
 										className="main-form__option">Phone</option>
-								<option value="Mail"
-										className="main-form__option">Mail</option>
-								<option value="Link"
+								<option value="email"
+										className="main-form__option">Email</option>
+								<option value="link"
 										className="main-form__option">Link</option>
 							</select>
 						</label>
-						<input
-							type={item.inputType}
-							name="inputInfo"
-							value={item.value}
-							onChange={event => this.handleInput (event, i)}
-						/>
-						<input
+						{(item.pattern !== "")
+							? <input
+								className="main-form__input"
+								type={item.inputType}
+								name="inputInfo"
+								value={item.value}
+								pattern={item.pattern}
+								onChange={event => this.handleInput (event, i)}
+							/>
+							: <input
+								className="main-form__input"
+								type={item.inputType}
+								name="inputInfo"
+								value={item.value}
+								onChange={event => this.handleInput (event, i)}
+							/>
+						}
+						{item.value !== "" || item.id === 0 || item.id !== this.state.data.length - 1
+							?	<button
+								type="button"
+								className="main-form__btn-add"
+								onClick={() => this.handleAddInput(i)}
+							>+</button>
+							: false
+						}
+						<button
 							type="button"
-							value="+"
-							onClick={() => this.handleAddInput(i)}
-						/>
-						<input
-							type="button"
-							value="-"
+							className="main-form__btn-remove"
 							onClick={() => this.handleRemoveInput(i)}
-						/>
+						>-</button>
 					</div>)})};
 				<br/><br/><br/>
-				<input type="submit" value="Submit" />
+				<button type="submit"
+						className="main-form__btn-submit"
+				>Submit</button>
 				<pre>
 					{JSON.stringify(this.state, null, 2)}
 				</pre>
